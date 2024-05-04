61d831ffc3af265b5a9fd48d7f08fb39d8937849
Awinas Kannan M R <Awinas.Kannan.Manicka.Rajan@walmart.com>
2024-05-04 21:59:41 +0530

    21

--- next-js-rendering/src/pages/users.js
+++ next-js-rendering/src/pages/users.js
@@ -1,6 +1,7 @@
 import User from '../components/user'
 
 function UserList({ users }) {
+  // function UserList(props) {
   return (
     <>
       <h1>List of users</h1>
@@ -22,7 +23,6 @@ export async function getStaticProps() {
   const response = await fetch('https://jsonplaceholder.typicode.com/users')
   const data = await response.json()
   //   console.log(data)
-
   return {
     props: {
       users: data
