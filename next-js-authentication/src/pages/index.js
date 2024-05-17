53a03abb21a98c0a9ede53d08e815216485b14b9
Awinas Kannan M R <Awinas.Kannan.Manicka.Rajan@walmart.com>
2024-05-18 00:21:48 +0530

    SessionProvider
    SessionProvider

--- next-js-authentication/src/pages/index.js
+++ next-js-authentication/src/pages/index.js
@@ -2,10 +2,12 @@ import Head from "next/head";
 import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
+import { useSession } from "next-auth/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
+  const { data: session } = useSession()
   return (
     <>
       <Head>
@@ -15,6 +17,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main className={`${styles.main} ${inter.className}`}>
+        <h1 className={styles.title}>
+          {session ? `${session.user.name}, ` : ''}Welcome to{' '}
+          <a href='https://nextjs.org'>Next.js!</a>
+        </h1>
         <div className={styles.description}>
           <p>
             Get started by editing&nbsp;
