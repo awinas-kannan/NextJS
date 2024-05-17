2d4c61c90a87a979b43cf47b8b4ba95339a3525e
Awinas Kannan M R <Awinas.Kannan.Manicka.Rajan@walmart.com>
2024-05-18 01:56:57 +0530

    Passing data to Client using Jwt and Session
    Passing data to Client using Jwt and Session

--- next-js-authentication/src/pages/api/auth/[...nextauth].js
+++ next-js-authentication/src/pages/api/auth/[...nextauth].js
@@ -9,5 +9,28 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  callbacks: {
+
+    async session({ session, user, token }) {
+      console.log('session session-> ', session)
+      console.log('session user-> ', user)
+      console.log('session token-> ', token)
+      session.user.login = token.id;
+      return session
+    },
+    async jwt({ token, user, account, profile, isNewUser }) {
+      console.log('NextAuth token-> ', token)
+      console.log('NextAuth user-> ', user)
+      console.log('NextAuth account-> ', account)
+      console.log('NextAuth profile-> ', profile)
+      console.log('NextAuth isNewUser-> ', isNewUser)
+      if (profile) {
+        console.log('Adding id in token -> ')
+        token.id = profile.login
+      }
+      return token
+    }
+  }
+
 }
 export default NextAuth(authOptions)
